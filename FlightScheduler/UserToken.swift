70d288e6b9a6a4afb5a946fe6b4a4e6510e6456b
Rochelle Lawrence <rochelle.lawrence@avast.com>
2020-03-24 15:54:05 +0100

    Changes made to the ViewController

---
+++ FlightScheduler/UserToken.swift
@@ -0,0 +1,9 @@
+//
+//  UserToken.swift
+//  FlightScheduler
+//
+//  Created by Rochelle Lawrence on 12/03/2020.
+//  Copyright © 2020 Practice. All rights reserved.
+//
+
+import Foundation
--- FlightScheduler/ViewController.swift
+++ FlightScheduler/ViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
     
-
+    @IBOutlet weak var submitButton: UIButton!
+    
     @IBAction func OriginButton(_ sender: UIButton) {
     }
     
@@ -18,13 +19,33 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
 
     @IBAction func SubmitButton(_ sender: UIButton) {
+        
+        
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        submitButton.addTarget(self, action: #selector(self.onTap(_:)), for: .touchUpInside)
     }
 
+
+
+   @objc func onTap(_ sender: AnyObject) -> Void {
+
+       let url = URL(string: "https://api.lufthansa.com/v1/oauth/token")!
+       let request = URLRequest(url: url)
+       let task = URLSession.shared.dataTask(with: request as URLRequest, completionHandler: { data, response, error in
+           //the service response is here
+            print (data)
+            print (response)
+            print (error)
+    
+    })
+    }
+    
+
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
      return 1
@@ -39,4 +60,5 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
     
 }
+
 
